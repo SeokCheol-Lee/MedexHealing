02ea72b350e6c89161754d3dafa9929f747c45f2
lsc <lsc991123@gmail.com>
2022-01-19 00:49:22 +0900

    splash test1

---
+++ app/src/main/java/org/techtown/medexhealing/PairingActivity.kt
@@ -0,0 +1,11 @@
+package org.techtown.medexhealing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PairingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pairing)
+    }
+}
