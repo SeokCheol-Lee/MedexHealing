c863e5db5cef1ea7883b3ec3a447ba8c904c9859
lsc <lsc991123@gmail.com>
2022-02-10 16:04:59 +0900

    회원가입 중복,공란 다이얼로그 추가

--- app/src/main/java/org/techtown/medexhealing/SingnUpActivity.kt
+++ app/src/main/java/org/techtown/medexhealing/SingnUpActivity.kt
@@ -1,5 +1,7 @@
 package org.techtown.medexhealing
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -15,6 +17,9 @@ class SingnUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        var isExistBlack = false
+        var isPWSame = false
+
         val rgbinding = ActivitySingnUpBinding.inflate(layoutInflater)
         setContentView(rgbinding.root)
 
@@ -29,33 +34,81 @@ class SingnUpActivity : AppCompatActivity() {
             var sir = rgbinding.etSiralnum.text.toString()
             var uid = rgbinding.etId.text.toString()
             var upw = rgbinding.etPw.text.toString()
+            var upc = rgbinding.etCheckpw.text.toString()
             var uph = rgbinding.etPhonenum.text.toString()
 
+            if(uid.isEmpty()||upw.isEmpty()||uph.isEmpty()||upc.isEmpty()){
+                isExistBlack = true
+            }
+            else{
+                if(upw == upc){
+                    isPWSame = true
+                }
+            }
 
-            Log.d("Main","serial: $sir id: $uid, pw: $upw, phone: $uph")
+            if(!isExistBlack && isPWSame){
+                Log.d("Main","serial: $sir id: $uid, pw: $upw, phone: $uph")
 
-            registerService.requestRegister(sir,uid,upw,uph).enqueue(object: Callback<Register> {
-                override fun onResponse(call: Call<Register>, response: Response<Register>) {
-                    val register = response.body()
-                    var dialog = AlertDialog.Builder(this@SingnUpActivity)
-                    Log.d("회원가입 성공","msg : "+register?.msg)
-                    Log.d("회원가입 성공","msg : "+register?.code)
-                    dialog.setTitle(register?.msg)
-                    dialog.setMessage(register?.code)
-                    dialog.show()
+                registerService.requestRegister(sir,uid,upw,uph).enqueue(object: Callback<Register> {
+                    override fun onResponse(call: Call<Register>, response: Response<Register>) {
+                        val register = response.body()
+                        var dialog = AlertDialog.Builder(this@SingnUpActivity)
+                        Log.d("회원가입 성공","msg : "+register?.msg)
+                        Log.d("회원가입 성공","msg : "+register?.code)
+                        dialog.setTitle(register?.msg)
+                        dialog.setMessage(register?.code)
+                        dialog.show()
+                        val intent = Intent(this@SingnUpActivity,LoginActivity::class.java)
+                        startActivity(intent)
+
+                    }
+
+                    override fun onFailure(call: Call<Register>, t: Throwable) {
+                        var dialog = AlertDialog.Builder(this@SingnUpActivity)
+                        Log.d("회원가입 실패","${t.localizedMessage}")
+                        dialog.setTitle("에러")
+                        dialog.setMessage("호출에 실패하였습니다")
+                        dialog.show()
+                    }
+
+                })
+            }
+            else{
+                if(isExistBlack){
+                    dialog("blank")
+                }
+                else if(!isPWSame){
+                    dialog("not same")
                 }
 
-                override fun onFailure(call: Call<Register>, t: Throwable) {
-                    var dialog = AlertDialog.Builder(this@SingnUpActivity)
-                    Log.d("회원가입 실패","${t.localizedMessage}")
-                    dialog.setTitle("에러")
-                    dialog.setMessage("호출에 실패하였습니다")
-                    dialog.show()
-                }
-
-            })
-
+            }
 
         }
+    }
+    fun dialog(type: String){
+        val dialog = AlertDialog.Builder(this)
+
+        // 작성 안한 항목이 있을 경우
+        if(type.equals("blank")){
+            dialog.setTitle("회원가입 실패")
+            dialog.setMessage("입력란을 모두 작성해주세요")
+        }
+        // 입력한 비밀번호가 다를 경우
+        else if(type.equals("not same")){
+            dialog.setTitle("회원가입 실패")
+            dialog.setMessage("비밀번호가 다릅니다")
+        }
+
+        val dialog_listener = object: DialogInterface.OnClickListener{
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                when(which){
+                    DialogInterface.BUTTON_POSITIVE ->
+                        Log.d("회원가입", "다이얼로그")
+                }
+            }
+        }
+
+        dialog.setPositiveButton("확인",dialog_listener)
+        dialog.show()
     }
 }
