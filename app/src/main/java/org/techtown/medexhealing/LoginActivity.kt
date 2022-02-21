39e69f4be88c6946a4296407b358316585cd11be
joseongbeom <drjeric@gmail.com>
2022-02-21 16:47:00 +0900

    메인으로 쓸 컬러 color 파일에 저장

--- app/src/main/java/org/techtown/medexhealing/LoginActivity.kt
+++ app/src/main/java/org/techtown/medexhealing/LoginActivity.kt
@@ -34,43 +34,42 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        var retrofit = Retrofit.Builder()
+            .baseUrl("http://220.149.236.48:3001/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-//        var retrofit = Retrofit.Builder()
-//            .baseUrl("http://220.149.236.48:3001/")
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//
-//        var loginService = retrofit.create(LoginService::class.java)
-//
-//        lgbinding.loginBtn.setOnClickListener{
-//            var uid = lgbinding.etLoginid.text.toString()
-//            var upw = lgbinding.etLoginpw.text.toString()
-//
-//
-//            Log.d("Main","id: $uid, pw: $upw")
-//
-//            loginService.requestLogin(uid,upw).enqueue(object: Callback<Login> {
-//                override fun onFailure(call: Call<Login>, t: Throwable) {
-//                    var dialog = AlertDialog.Builder(this@LoginActivity)
-//                    Log.d("로그인 실패","${t.localizedMessage}")
-//                    dialog.setTitle("에러")
-//                    dialog.setMessage("호출에 실패하였습니다")
-//                    dialog.show()
-//                }
-//
-//                override fun onResponse(call: Call<Login>, response: Response<Login>) {
-//                    val login = response.body()
-//                    var dialog = AlertDialog.Builder(this@LoginActivity)
-//                    Log.d("로그인 성공","msg : "+login?.msg)
-//                    Log.d("로그인 성공","msg : "+login?.code)
-//                    dialog.setTitle(login?.msg)
-//                    dialog.setMessage(login?.code)
-//                    dialog.show()
-//
-//                }
-//            })
-//
-//        }
+        var loginService = retrofit.create(LoginService::class.java)
+
+        lgbinding.loginBtn.setOnClickListener{
+            var uid = lgbinding.etLoginid.text.toString()
+            var upw = lgbinding.etLoginpw.text.toString()
+
+
+            Log.d("Main","id: $uid, pw: $upw")
+
+            loginService.requestLogin(uid,upw).enqueue(object: Callback<Login> {
+                override fun onFailure(call: Call<Login>, t: Throwable) {
+                    var dialog = AlertDialog.Builder(this@LoginActivity)
+                    Log.d("로그인 실패","${t.localizedMessage}")
+                    dialog.setTitle("에러")
+                    dialog.setMessage("호출에 실패하였습니다")
+                    dialog.show()
+                }
+
+                override fun onResponse(call: Call<Login>, response: Response<Login>) {
+                    val login = response.body()
+                    var dialog = AlertDialog.Builder(this@LoginActivity)
+                    Log.d("로그인 성공","msg : "+login?.msg)
+                    Log.d("로그인 성공","msg : "+login?.code)
+                    dialog.setTitle(login?.msg)
+                    dialog.setMessage(login?.code)
+                    dialog.show()
+
+                }
+            })
+
+        }
 
 
     }
--- app/src/main/java/org/techtown/medexhealing/LoginService.kt
+++ app/src/main/java/org/techtown/medexhealing/LoginService.kt
@@ -1,9 +1,11 @@
 package org.techtown.medexhealing
 
+
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
+
 
 interface LoginService {
 
