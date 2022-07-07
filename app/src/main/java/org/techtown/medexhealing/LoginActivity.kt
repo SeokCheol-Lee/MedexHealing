f3629a3bfac6b6219bbcb4fa3f742ff3e1de2dad
lsc <lsc991123@gmail.com>
2022-07-07 16:11:48 +0900

    알람 추가 및 자동로그인 수정

--- app/src/main/java/org/techtown/medexhealing/LoginActivity.kt
+++ app/src/main/java/org/techtown/medexhealing/LoginActivity.kt
@@ -23,26 +23,17 @@ class LoginActivity : AppCompatActivity() {
         val lgbinding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(lgbinding.root)
 
-        if(lgbinding.autologin.isChecked){
-
-            // SharedPreferences 안에 값이 저장되어 있을 때 -> MainActivity로 이동
-            //Toast.makeText(this, "${MySharedPreferences.getUserId(this)}님 자동 로그인 되었습니다.", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-
         lgbinding.fdpass.setOnClickListener {
             val intent = Intent(this,FindActivity::class.java)
             startActivity(intent)
         }
-        /*
+/*
         lgbinding.loginBtn.setOnClickListener {
-            val intent = Intent(this, UserSelectActivity::class.java)
+            val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
-         */
+*/
+
         lgbinding.btnRegister.setOnClickListener {
             val intent = Intent(this,SingnUpActivity::class.java)
             startActivity(intent)
@@ -55,11 +46,54 @@ class LoginActivity : AppCompatActivity() {
 
         var loginService = retrofit.create(LoginService::class.java)
 
+        if(MySharedPreferences.getUserCheck(this) == true){
+            var dialog = AlertDialog.Builder(this@LoginActivity)
+            Log.d("자동로그인","체크확인")
+            var uid = MySharedPreferences.getUserId(this)
+            var upw = MySharedPreferences.getUserPass(this)
+            val intent = Intent(this,HomeActivity::class.java)
+            Log.d("자동로그인","id: $uid, pw: $upw")
+            loginService.requestLogin(uid,upw).enqueue(object: Callback<Login> {
+                override fun onFailure(call: Call<Login>, t: Throwable) {
+                    Log.d("로그인 실패","${t.localizedMessage}")
+                    dialog.setTitle("에러")
+                    dialog.setMessage("호출에 실패하였습니다")
+                    dialog.show()
+                }
+
+                override fun onResponse(call: Call<Login>, response: Response<Login>) {
+                    val login = response.body()
+
+                    if(login?.code == 200){
+                        Log.d("로그인 성공","msg : "+login?.msg)
+                        Log.d("로그인 성공","code : "+login?.code)
+                        MySharedPreferences.setUserId(this@LoginActivity,uid)
+                        MySharedPreferences.setUserPass(this@LoginActivity,upw)
+                        startActivity(intent)
+                        finish()
+                    }
+                    else {
+                        MySharedPreferences.clearUser(this@LoginActivity)
+                        Log.d("자동로그인","유저삭제 전 id: $uid, pw: $upw")
+                        Log.d("자동로그인","유저삭제")
+                        Log.d("존재하지 않는 아이디","msg : "+login?.msg)
+                        Log.d("존재하지 않는 아이디","code : "+login?.code)
+                        dialog.setTitle("로그인 실패")
+                        dialog.setMessage("존재하지 않는 아이디입니다")
+                        dialog.show()
+                    }
+
+                }
+            })
+        }
+
         lgbinding.loginBtn.setOnClickListener{
-            var uid = lgbinding.etLoginid.text.toString()
-            var upw = lgbinding.etLoginpw.text.toString()
             val intent = Intent(this,HomeActivity::class.java)
             var dialog = AlertDialog.Builder(this@LoginActivity)
+
+
+            var uid = lgbinding.etLoginid.text.toString()
+            var upw = lgbinding.etLoginpw.text.toString()
 
             if(uid.isEmpty()){
                 dialog.setTitle("로그인 실패")
@@ -71,16 +105,20 @@ class LoginActivity : AppCompatActivity() {
                 dialog.setMessage("비밀번호를 입력하세요")
                 dialog.show()
             }
-            else{
-                dialog.setTitle("에러")
-                dialog.setMessage("로그인에 실패하였습니다")
-                dialog.show()
-            }
-
-            //startActivity(intent)
-
 
             Log.d("Main","id: $uid, pw: $upw")
+
+            if(lgbinding.autologin.isChecked){
+                MySharedPreferences.setUserCheck(this,true)
+                Log.d("자동로그인","체크됨")
+                MySharedPreferences.setUserId(this@LoginActivity,uid)
+                MySharedPreferences.setUserPass(this@LoginActivity,upw)
+                Log.d("자동로그인","유저정보 저장")
+            }
+            else{
+                MySharedPreferences.setUserCheck(this,false)
+                Log.d("자동로그인","체크안됨")
+            }
 
             loginService.requestLogin(uid,upw).enqueue(object: Callback<Login> {
                 override fun onFailure(call: Call<Login>, t: Throwable) {
@@ -92,23 +130,17 @@ class LoginActivity : AppCompatActivity() {
 
                 override fun onResponse(call: Call<Login>, response: Response<Login>) {
                     val login = response.body()
-                    var dialog = AlertDialog.Builder(this@LoginActivity)
 
-
-                    if(login?.code == 100){
+                    if(login?.code == 200){
                         Log.d("로그인 성공","msg : "+login?.msg)
-                        Log.d("로그인 성공","msg : "+login?.code)
-                        MySharedPreferences.setUserId(this@LoginActivity,lgbinding.etLoginid.toString())
-                        MySharedPreferences.setUserPass(this@LoginActivity,lgbinding.etLoginpw.toString())
-                        dialog.setTitle(login?.msg)
-                        dialog.setMessage(login?.code)
-                        dialog.show()
+                        Log.d("로그인 성공","code : "+login?.code)
                         startActivity(intent)
                         finish()
                     }
                     else {
+                        MySharedPreferences.removeUser(this@LoginActivity)
                         Log.d("존재하지 않는 아이디","msg : "+login?.msg)
-                        Log.d("존재하지 않는 아이디","msg : "+login?.code)
+                        Log.d("존재하지 않는 아이디","code : "+login?.code)
                         dialog.setTitle("로그인 실패")
                         dialog.setMessage("존재하지 않는 아이디입니다")
                         dialog.show()
